8491cf334fe00a60124058e432973d0fb02f081f
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-12-17 08:56:42 -0500

    🔨 [ADD] product controller

---
+++ api/src/main/java/com/truek/api/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.truek.api.controller;
+
+import com.truek.api.dtos.ProductDTO;
+import com.truek.api.request.ProductRequest;
+import com.truek.api.response.ApiResponse;
+import com.truek.api.services.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping("/app")
+@RequiredArgsConstructor
+public class ProductController {
+  private final ProductService productService;
+
+  @PostMapping(value = "add/product" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<ApiResponse<ProductDTO>> addProduct(@ModelAttribute ProductRequest productRequest) {
+    try {
+      ProductDTO productDTO = productService.addProduct(productRequest);
+      return ResponseEntity.ok(new ApiResponse<>("producto añadido", productDTO));
+    }catch (Exception e){
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(e.getMessage(), null));
+    }
+  }
+}
