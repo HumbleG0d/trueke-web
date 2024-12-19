b6c9997b4b9025c8fe29da386222e8423dbd9504
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-12-19 16:26:05 -0500

    🔧 [ADD] the answers to search for products, by categories

--- api/src/main/java/com/truek/api/controller/ProductController.java
+++ api/src/main/java/com/truek/api/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.truek.api.controller;
 
 import com.truek.api.dtos.ProductDTO;
+import com.truek.api.entity.Categories;
 import com.truek.api.request.ProductRequest;
 import com.truek.api.response.ApiResponse;
 import com.truek.api.services.product.ProductService;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -25,4 +28,25 @@ public class ProductController {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(e.getMessage(), null));
     }
   }
+
+  @GetMapping("/all/products")
+  public ResponseEntity<ApiResponse<List<ProductDTO>>> getAllProducts() {
+    try {
+      var products = productService.getAllProducts();
+      return ResponseEntity.ok(new ApiResponse<>("Productos mostrados", products));
+    }catch (Exception e){
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(null, null));
+    }
+  }
+
+  @GetMapping("/all/products/by-category")
+  public ResponseEntity<ApiResponse<List<ProductDTO>>> getAllProductsByCategory(@RequestParam String category) {
+    try {
+      var products = productService.getProductsByCategory(category.toUpperCase());
+      return ResponseEntity.ok(new ApiResponse<>("Productos mostrados", products));
+    }catch (Exception e){
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(null, null));
+    }
+  }
+
 }
