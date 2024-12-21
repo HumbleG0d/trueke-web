124e70c4db189e97a48e8a51320b380a0faa8305
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-12-20 23:58:19 -0500

    [CHANGE] request TradeProductDTO

--- api/src/main/java/com/truek/api/dtos/TradeProductDTO.java
+++ api/src/main/java/com/truek/api/dtos/TradeProductDTO.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record TradeProductDTO(
-        @NotNull
-        Long id,
         @NotBlank
         String name
 ) {
