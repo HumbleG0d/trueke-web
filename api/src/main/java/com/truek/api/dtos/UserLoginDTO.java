531518817a28ec63ecbc2711182ee3ac86c2a4c8
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-12-21 00:51:29 -0500

    🔧 [ADD] data userlogin in  response login

--- api/src/main/java/com/truek/api/dtos/UserLoginDTO.java
+++ api/src/main/java/com/truek/api/dtos/UserLoginDTO.java
@@ -1,11 +1,11 @@
 package com.truek.api.dtos;
 
+import com.truek.api.response.UserRegisterResponse;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 public record UserLoginDTO(
-        @Email
-        String email,
+        UserRegisterResponse userLogin,
         @NotNull
         String token
 ) {
